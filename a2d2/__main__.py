f703e8803c6caa9fd16942a236b99f36e219063f
Sami Remes <sami.remes@gmail.com>
2019-11-30 23:04:44 +0200

    Create main entry point to run the TFRecord writer

---
+++ a2d2/__main__.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+
+import a2d2.data_loader
+
+
+def write_tf_records(args):
+    writer = a2d2.data_loader.A2D2TFRecordWriter(args.output, os.path.join(args.datadir, "class_list.json"))
+    writer.write_images_to_tf_records(args.datadir)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run various utilities and training with the A2D2 dataset")
+    sub_parsers = parser.add_subparsers(required=True)
+
+    # Handle creating TFRecords
+    parser_tf_records = sub_parsers.add_parser("write_tf_records")
+    parser_tf_records.add_argument("--datadir", default="camera_lidar_semantic")
+    parser_tf_records.add_argument("--output", default="records")
+    parser_tf_records.set_defaults(function=write_tf_records)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    args.function(args)
