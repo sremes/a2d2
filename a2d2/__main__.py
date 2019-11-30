f562afb4bde7f6b686218b0bd9bae0be34547408
Sami Remes <sami.remes@gmail.com>
2019-11-30 23:44:59 +0200

    Fix pylint issues in main and data loader

--- a2d2/__main__.py
+++ a2d2/__main__.py
@@ -1,3 +1,5 @@
+"""Main entrypoints for the module are defined here."""
+
 import argparse
 import os
 
@@ -5,11 +7,13 @@ import a2d2.data_loader
 
 
 def write_tf_records(args):
+    """Run TFRecord writer with given arguments."""
     writer = a2d2.data_loader.A2D2TFRecordWriter(args.output, os.path.join(args.datadir, "class_list.json"))
     writer.write_images_to_tf_records(args.datadir)
 
 
 def parse_arguments():
+    """Argument parser with different sub-commands."""
     parser = argparse.ArgumentParser(description="Run various utilities and training with the A2D2 dataset")
     sub_parsers = parser.add_subparsers(required=True)
 
@@ -22,6 +26,11 @@ def parse_arguments():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
+    """Main function parses arguments and runs the chosen sub-command."""
     args = parse_arguments()
     args.function(args)
+
+
+if __name__ == "__main__":
+    main()
